d6a34e15dfaf308c7e7809320f69120034a853f3
Борисевич Виталий Александрович <bva@isc.by>
2020-04-21 16:48:10 +0300

    add handlers for playing page

---
+++ src/modules/cardsClickHandler.js
@@ -0,0 +1,23 @@
+import rotateCard from './rotateCard';
+import cards from './cards';
+import statesObj from './statesObj';
+
+
+const cardsClickHandler = (event) => {
+  event.preventDefault();
+
+  const targetRotateButton = event.target.closest('.rotate-button');
+  const target = event.target.closest('.card_play-mode');
+  const currentCardCollection = document.querySelectorAll('.card_play-mode');
+  const k = Array.from(currentCardCollection).indexOf(target);
+
+  if (targetRotateButton) {
+    rotateCard(targetRotateButton);
+  } else if (target) {
+    if (!statesObj.locked) {
+      new Audio(`./assets/${cards[statesObj.targetCollection][k].audioSrc}`).play();
+    }
+  }
+};
+
+export default cardsClickHandler;
